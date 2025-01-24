b55369102982675547db5e1820e8ff13a80ba64e
kimgihye <kimgihye55@gamil.com>
2025-01-24 18:05:05 +0900

    전체 레이아웃 완성/ 반응형 페이지 예정

--- js/style.js
+++ js/style.js
@@ -30,27 +30,63 @@ $(document).ready(function () {
   });
   // 프로모션 슬라이드
   var swiper = new Swiper(".proSwiper", {
-    autoplay: {
-      delay: 2000, // 자동 슬라이드 간격 (밀리초, 3000ms = 3초)
-      disableOnInteraction: false, // 사용자가 슬라이드를 조작해도 자동 슬라이드가 계속 작동
-    },
-    loop: true, // 슬라이드가 마지막에 도달하면 처음으로 되돌아감
-    slidesPerView: 1,
-    spaceBetween: 0,
+    slidesPerView: 1.5,
+    spaceBetween: 10,
     pagination: {
       el: ".proSwiper .swiper-pagination",
       clickable: true,
     },
     breakpoints: {
-      390: {
-        slidesPerView: 1,
-        spaceBetween: 5,
+      480: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
       },
       786: {
+        slidesPerView: 3.5,
+        spaceBetween: 10,
+      },
+    },
+  });
+  //메뉴 슬라이드
+  var swiper = new Swiper(".menuSwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    pagination: {
+      el: ".menuSwiper .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      480: {
         slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+      786: {
+        slidesPerView: 3.5,
         spaceBetween: 10,
       },
     },
   });
 });
- 
+window.onload = function () {
+  const footer = document.querySelector(".footer");
+  const topButton = document.querySelector('.top-btn-wrap');
+  // 스크롤 이벤트
+  window.addEventListener("scroll", () => {
+    // 스크롤 위치가 300px 이상이면 버튼 표시
+    if (window.scrollY > 200) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+
+    // 푸터와 겹치지 않도록 버튼 위치 조정
+    const footerRect = footer.getBoundingClientRect();
+    // const buttonHeight = topButton.offsetHeight;
+
+    if (footerRect.top < window.innerHeight) {
+      topButton.style.bottom = `${window.innerHeight - footerRect.top + 50}px`;
+    } else {
+      topButton.style.bottom = "50px";
+    }
+  });
+};
